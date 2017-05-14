6e4e14d5ce4872e158a8ab0d9c9dc680ec418281
William Thomas <wthoma390@gmail.com>
2017-05-14 14:40:25 -0600

    add destoy method and update link in show erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,10 +19,17 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
   private
 
   def article_params
     params.require(:article).permit(:title, :body)
   end
-  
+
 end
