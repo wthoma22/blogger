fff81a6e06f9b9c8ac540037428db99a9168b829
William Thomas <wthoma390@gmail.com>
2017-05-14 17:33:39 -0600

    update tag index and methods

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,3 +1,11 @@
 class TagsController < ApplicationController
-  @tag = Tag.find(params[:id])
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+    @tags = Tag.all
+  end
+
 end
